d84fcd119537790299cd9d27adefc152cc2010fa
nibin <nibinbiju222@gmail.com>
2023-12-28 11:42:37 +0530

    map updated

--- lib/view/auth/login.dart
+++ lib/view/auth/login.dart
@@ -1,4 +1,5 @@
 import 'package:doctor_baby/view/auth/registration.dart';
+import 'package:doctor_baby/view/homepage/map.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:fluttertoast/fluttertoast.dart';
@@ -7,6 +8,7 @@ class Login extends StatefulWidget {
   @override
   State<Login> createState() => _Login();
 }
+
 class _Login extends State<Login> {
   final email = TextEditingController();
   final pwd = TextEditingController();
@@ -22,13 +24,14 @@ class _Login extends State<Login> {
     super.initState();
   }
 
-  void check_if_user_already_login() async{
+  void check_if_user_already_login() async {
     preferences = await SharedPreferences.getInstance();
 
     newuser = preferences.getBool('newuser') ?? true;
-    
-    if(newuser == false){
-      Navigator.push(context, MaterialPageRoute(builder: (context)=>RegPage()));
+
+    if (newuser == false) {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => RegPage()));
     }
   }
 
@@ -38,10 +41,12 @@ class _Login extends State<Login> {
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.grey[300],
-        title: const Text("LOGIN PAGE",style: TextStyle(fontSize: 26, color: Colors.black, fontWeight: FontWeight.bold)),
+        title: const Text("LOGIN PAGE",
+            style: TextStyle(
+                fontSize: 26,
+                color: Colors.black,
+                fontWeight: FontWeight.bold)),
       ),
-
-
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Card(
@@ -52,83 +57,79 @@ class _Login extends State<Login> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-            
-              const SizedBox(height: 25),
-            
-              Text("Welcome User", style: TextStyle(color: Colors.blue, fontSize: 25),),
-            
-                      const SizedBox(height: 25),  
-            
-                      Text("Email",),
-            
-                        SizedBox(height: 5),
-            
-                        Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(25)
-                            ),
-                            child: TextField(
-                              controller: email,
-                              style: TextStyle(color: Colors.grey[600]),
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(5)
-                                ),
-                                isDense: true,
-                                hintText: "Enter Email",
-                                fillColor: Colors.green
-                              ),
-                            ),
-                          ),
-                      const SizedBox(height: 25),  
-            
-                      Text("Password",),
-            
-                        SizedBox(height: 5),
-            
-                        Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(25)
-                            ),
-                            child: TextField(
-                              controller: pwd,
-                              style: TextStyle(color: Colors.grey[600]),
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(5)
-                                ),
-                                isDense: true,
-                                hintText: "Enter Password",
-                                fillColor: Colors.green
-                              ),
-                            ),
-                          ),
-                      const SizedBox(height: 25),  
-            
+                const SizedBox(height: 25),
+                Text(
+                  "Welcome User",
+                  style: TextStyle(color: Colors.blue, fontSize: 25),
+                ),
+                const SizedBox(height: 25),
+                Text(
+                  "Email",
+                ),
+                SizedBox(height: 5),
+                Container(
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(25)),
+                  child: TextField(
+                    controller: email,
+                    style: TextStyle(color: Colors.grey[600]),
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                        isDense: true,
+                        hintText: "Enter Email",
+                        fillColor: Colors.green),
+                  ),
+                ),
+                const SizedBox(height: 25),
+                Text(
+                  "Password",
+                ),
+                SizedBox(height: 5),
+                Container(
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(25)),
+                  child: TextField(
+                    controller: pwd,
+                    style: TextStyle(color: Colors.grey[600]),
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                        isDense: true,
+                        hintText: "Enter Password",
+                        fillColor: Colors.green),
+                  ),
+                ),
+                const SizedBox(height: 25),
                 Padding(
                   padding: const EdgeInsets.only(left: 35),
                   child: ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.blue[700]),
-                  fixedSize: MaterialStateProperty.all(Size(250, 40)),
-                    ),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.blue[700]),
+                        fixedSize: MaterialStateProperty.all(Size(250, 40)),
+                      ),
                       onPressed: () async {
                         preferences = await SharedPreferences.getInstance();
                         String username = email.text;
                         String password = pwd.text;
-        
-                          setState(() {
-                          storedemail = preferences.getString('email')!;
-                          storedpass = preferences.getString('pass')!; 
-                        });
-        
 
-                        if ((username != "" && username == storedemail) && (password != "" && password == storedpass)) {
+                        setState(() {
+                          storedemail = preferences.getString('email')!;
+                          storedpass = preferences.getString('pass')!;
+                        });
+
+                        if ((username != "" && username == storedemail) &&
+                            (password != "" && password == storedpass)) {
                           preferences.setString('uname', username);
                           preferences.setString('pword', password);
                           preferences.setBool('newuser', false);
 
-                          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => RegPage())); // Navigate to RegPage after successful login
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      MapScreen())); // Navigate to RegPage after successful login
                         } else {
                           Fluttertoast.showToast(
                             msg: "Invalid Username or Password",
@@ -144,12 +145,13 @@ class _Login extends State<Login> {
                       },
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child: const Text("Login", style: TextStyle(color: Colors.white),),
+                        child: const Text(
+                          "Login",
+                          style: TextStyle(color: Colors.white),
+                        ),
                       )),
                 ),
-
                 SizedBox(height: 20),
-
                 Container(
                   alignment: Alignment.center,
                   width: double.infinity,
@@ -159,7 +161,7 @@ class _Login extends State<Login> {
                       Expanded(
                         child: Divider(
                           color: Colors.grey,
-                          height: 1,          
+                          height: 1,
                         ),
                       ),
                       Padding(
@@ -168,28 +170,34 @@ class _Login extends State<Login> {
                       ),
                       Expanded(
                         child: Divider(
-                          color: Colors.grey, 
-                          height: 1,           
+                          color: Colors.grey,
+                          height: 1,
                         ),
                       ),
                     ],
                   ),
                 ),
-
-                SizedBox(height: 20,),
-
-                Container(padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
                   decoration: BoxDecoration(
                     border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(20),),
-                  child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                       Image.asset("assets/google.png",height: 25),
-                       SizedBox(width: 25),
-                       Text("Signin with Google", style: TextStyle(fontSize: 16),),
+                      Image.asset("assets/google.png", height: 25),
+                      SizedBox(width: 25),
+                      Text(
+                        "Signin with Google",
+                        style: TextStyle(fontSize: 16),
+                      ),
                     ],
-                  ),),
-
+                  ),
+                ),
               ],
             ),
           ),
@@ -197,5 +205,4 @@ class _Login extends State<Login> {
       ),
     );
   }
-  
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_image_compress_macos
+import geolocator_apple
+import location
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
